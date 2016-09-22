2764a17c8eea89f61bbb9a18035d62ba4dd6bcd0
Frederick Chapleau <fchapleau@curchy.com>
2016-09-22 00:29:26 -0400

    Conversion to WebAPI & Direct File Download

---
+++ AzureRange.Website/Controllers/DownloadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System.Net.Http;
+
+namespace AzureRange.Website.Controllers
+{
+    public class DownloadController : BaseController
+    {
+        public ActionResult Index(string[] region, string outputformat, string command, bool complement = false)
+        {
+            int resultCount;
+            var resultString = GenerationHelper.Generate(region, outputformat, false, out resultCount);
+            var outputFileName = "Results-UTC-" + DateTime.UtcNow + "-" + string.Join("-", region) + ".txt";
+            
+            return File(Encoding.UTF8.GetBytes(resultString), "application/octet-stream", outputFileName);
+        }
+    }
+}
